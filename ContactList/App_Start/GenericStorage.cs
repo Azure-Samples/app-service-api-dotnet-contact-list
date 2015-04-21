ad7fe081e050461435f85e6b531ae67cdc8dbe01
bradygaster <brady.gaster@gmail.com>
2015-04-21 09:39:13 -0700

    added isolated storage

---
+++ ContactList/App_Start/GenericStorage.cs
@@ -0,0 +1,37 @@
+﻿using ContactList.Models;
+using Microsoft.Azure.AppService.ApiApps.Service;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    public class GenericStorage
+    {
+        CloudIsolatedStorage storage;
+
+        public GenericStorage()
+        {
+            storage = Runtime.FromAppSettings().IsolatedStorage;
+        }
+
+        public async Task Save(IEnumerable<Contact> target, string filename)
+        {
+            var json = JsonConvert.SerializeObject(target);
+            var data = Encoding.ASCII.GetBytes(json);
+            await storage.WriteAsync(filename, data);
+        }
+
+        public async Task<IEnumerable<Contact>> Get(string filename)
+        {
+            var json = await storage.ReadAsStringAsync(filename);
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            return JsonConvert.DeserializeObject<IEnumerable<Contact>>(json);
+        }
+    }
+}
--- ContactList/Controllers/ContactsController.cs
+++ ContactList/Controllers/ContactsController.cs
@@ -1,28 +1,76 @@
 ﻿using ContactList.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using System.Linq;
 
 namespace ContactList.Controllers
 {
     [EnableCors(origins:"*", headers:"*", methods: "*")] /* -- NEW CODE -- */
     public class ContactsController : ApiController
     {
-        [HttpGet]
-        public IEnumerable<Contact> Get()
+        private const string FILENAME = "contacts.json";
+        private GenericStorage _storage;
+
+        public ContactsController()
         {
-            return new Contact[]
+            _storage = new GenericStorage();
+        }
+
+        private async Task<IEnumerable<Contact>> GetContact()
+        {
+            var contacts = await _storage.Get(FILENAME);
+
+            if (contacts == null)
             {
-                new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
-                new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
-                new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"}
-            };
+                await _storage.Save(new Contact[]{
+                        new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
+                        new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
+                        new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"}
+                    }
+                , FILENAME);
+            }
+
+            return contacts;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(IEnumerable<Contact>))]
+        public async Task<IEnumerable<Contact>> Get()
+        {
+            return await GetContact();
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(Contact))]
+        public async Task<Contact> GetById([FromUri] int id)
+        {
+            var contacts = await GetContact();
+            return contacts.FirstOrDefault(x => x.Id == id);
         }
 
         [HttpPost]
-        public Contact Post([FromBody] Contact contact)
+        [ResponseType(typeof(Contact))]
+        public async Task<Contact> Post([FromBody] Contact contact)
         {
+            var contacts = await GetContact();
+            var contactList = contacts.ToList();
+            contactList.Add(contact);
+            await _storage.Save(contactList, FILENAME);
             return contact;
+        }
+
+        [HttpDelete]
+        [ResponseType(typeof(bool))]
+        public async Task<bool> Delete([FromUri] int id)
+        {
+            var contacts = await GetContact();
+            var contactList = contacts.ToList();
+            contactList.RemoveAll(x => x.Id == id);
+            await _storage.Save(contactList, FILENAME);
+            return true;
         }
     }
 }
