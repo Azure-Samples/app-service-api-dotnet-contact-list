1db52c2c7d6752bf5ebd7c2a35ce872f918d2f47
bradygaster <brady.gaster@gmail.com>
2015-04-20 17:59:00 -0700

    initial creation

---
+++ ContactList/Controllers/ContactsController.cs
@@ -0,0 +1,26 @@
+﻿using ContactList.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ContactList.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return new Contact[]
+            {
+                new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
+                new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
+                new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"}
+            };
+        }
+
+        [HttpPost]
+        public Contact Post([FromBody] Contact contact)
+        {
+            return contact;
+        }
+    }
+}
---
+++ ContactList/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ContactList.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
