f7e69e7795a1cd596e4ab94334172eacf64a230f
Tom Dykstra <tdykstra@microsoft.com>
2015-11-29 21:21:14 -0800

    add projects for .net client and auth

---
+++ CompanyContacts.API/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CompanyContacts.API.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ ContactsList.Angular.AAD/app/scripts/contactsSvc.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+angular.module('contactsListApp')
+.factory('contactsSvc', ['$http', function ($http) {
+    //var apiEndpoint = "https://{your api app name}.azurewebsites.net";
+    var apiEndpoint = "https://localhost:44300";
+
+    $http.defaults.useXDomain = true;
+    delete $http.defaults.headers.common['X-Requested-With']; 
+
+    return {
+        getItems: function () {
+            return $http.get(apiEndpoint + '/api/contacts');
+        },
+        getItem : function(id){
+            return $http.get(apiEndpoint + '/api/contacts/' + id);
+        },
+        postItem : function(item){
+            return $http.post(apiEndpoint + '/api/contacts', item);
+        },
+        putItem : function(item){
+            return $http.put(apiEndpoint + '/api/contacts/', item);
+        },
+        deleteItem : function(id){
+            return $http({
+                method: 'DELETE',
+                url: apiEndpoint + '/api/contacts/' + id
+            });
+        }
+    };
+}]);
